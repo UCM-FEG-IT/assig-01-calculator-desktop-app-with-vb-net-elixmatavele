ecbcd61919d353f6b8cf12d27c26577b9a8b23c5
elixmatavele <47817789+elixmatavele@users.noreply.github.com>
2019-02-28 12:56:32 +0200

    Calculadora
    Versao 1

---
+++ Calculadura/Calculadura/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Dim button1 As Button = CType(sender, Button)
+
+        If button1.Name = "Button0" Then
+            TextBox1.Text = TextBox1.Text + "0"
+
+
+        End If
+    End Sub
+
+    Private Sub Button1ClickMethodo(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim button1 As Button = CType(sender, Button)
+
+        If button1.Name = "Button1" Then
+            TextBox1.Text = TextBox1.Text + "1"
+
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim button1 As Button = CType(sender, Button)
+
+        If button1.Name = "Button2" Then
+            TextBox1.Text = TextBox1.Text + "2"
+
+
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim button1 As Button = CType(sender, Button)
+
+        If button1.Name = "Button3" Then
+            TextBox1.Text = TextBox1.Text + "3"
+
+
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim button1 As Button = CType(sender, Button)
+
+        If button1.Name = "Button4" Then
+            TextBox1.Text = TextBox1.Text + "4"
+
+
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim button1 As Button = CType(sender, Button)
+
+        If button1.Name = "Button5" Then
+            TextBox1.Text = TextBox1.Text + "5"
+
+
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim button1 As Button = CType(sender, Button)
+
+        If button1.Name = "Button6" Then
+            TextBox1.Text = TextBox1.Text + "6"
+
+
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim button1 As Button = CType(sender, Button)
+
+        If button1.Name = "Button7" Then
+            TextBox1.Text = TextBox1.Text + "7"
+
+
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim button1 As Button = CType(sender, Button)
+
+        If button1.Name = "Button8" Then
+            TextBox1.Text = TextBox1.Text + "8"
+
+
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim button1 As Button = CType(sender, Button)
+
+        If button1.Name = "Button9" Then
+            TextBox1.Text = TextBox1.Text + "9"
+
+
+        End If
+    End Sub
+End Class
